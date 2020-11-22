e50a7f305892cd2d87a4c1f4d0a8a4a9e669551d
Andres Ceron <andres.ceron@me.com>
2020-11-22 05:31:18 +0100

    DIG-28 moving express.static

--- server/src/loaders/express.ts
+++ server/src/loaders/express.ts
@@ -50,11 +50,11 @@ export default ({ app }: { app: express.Application }) => {
   const distDir = path.join(__dirname, '../../../ui/dist/angularnode/');
   console.log(':: distDir :: ', distDir);
 
-  app.get('*', (req, res) => {
-    res.sendFile(distDir);
-  });
+  app.use(express.static(distDir));
 
-  // app.use(express.static(distDir));
+  // app.get('*', (req, res) => {
+  //   res.sendFile(distDir);
+  // });
 
   /** catch 404 and forward to error handler */
   app.use((req: Request, res: Response, next: NextFunction) => {
