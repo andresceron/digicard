7c13e455eee627962f4223cfefa99f99009780a3
Andres Ceron <andres.ceron@me.com>
2020-11-22 18:11:34 +0100

    DIG-28 change routing..

--- server/src/loaders/express.ts
+++ server/src/loaders/express.ts
@@ -47,8 +47,14 @@ export default ({ app }: { app: express.Application }) => {
   // /** Mount all routes on /api path */
   app.use('/api', routes);
 
-  // /** Mount routes for UI */
-  app.use('/*', clientRoutes);
+  // Serve UI dist
+  const distDir = path.join(__dirname, '../../../ui/dist/angularnode/');
+  app.use(express.static(distDir));
+  console.log(':: distDir :: ', distDir);
+
+  app.get('/*', (req, res) => {
+    res.sendFile(distDir);
+  });
 
   /** catch 404 and forward to error handler */
   app.use((req: Request, res: Response, next: NextFunction) => {
